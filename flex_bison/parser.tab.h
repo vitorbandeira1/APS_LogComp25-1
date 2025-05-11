edb2fa62cf9be54f6db3c0ef5c941453393d782b
vitorbandeira1 <vitorirulegui@gmail.com>
2025-05-11 18:20:26 -0300

    Parcial 2

--- flex_bison/parser.tab.h
+++ flex_bison/parser.tab.h
@@ -1,8 +1,8 @@
-/* A Bison parser, made by GNU Bison 3.8.2.  */
+/* A Bison parser, made by GNU Bison 3.5.1.  */
 
 /* Bison interface for Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2021 Free Software Foundation,
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2020 Free Software Foundation,
    Inc.
 
    This program is free software: you can redistribute it and/or modify
@@ -16,7 +16,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -31,9 +31,8 @@
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
-/* DO NOT RELY ON FEATURES THAT ARE NOT DOCUMENTED in the manual,
-   especially those whose name start with YY_ or yy_.  They are
-   private implementation details that can be changed or removed.  */
+/* Undocumented macros, especially those whose name start with YY_,
+   are private implementation details.  Do not rely on them.  */
 
 #ifndef YY_YY_PARSER_TAB_H_INCLUDED
 # define YY_YY_PARSER_TAB_H_INCLUDED
@@ -45,50 +44,45 @@
 extern int yydebug;
 #endif
 
-/* Token kinds.  */
+/* Token type.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
   enum yytokentype
   {
-    YYEMPTY = -2,
-    YYEOF = 0,                     /* "end of file"  */
-    YYerror = 256,                 /* error  */
-    YYUNDEF = 257,                 /* "invalid token"  */
-    SUBMERGIR = 258,               /* SUBMERGIR  */
-    EMERGIR = 259,                 /* EMERGIR  */
-    AJUSTAR_INCLINACAO = 260,      /* AJUSTAR_INCLINACAO  */
-    ATIVAR_PROPULSOR = 261,        /* ATIVAR_PROPULSOR  */
-    AJUSTAR_PROFUNDIDADE = 262,    /* AJUSTAR_PROFUNDIDADE  */
-    AJUSTAR_POSICAO = 263,         /* AJUSTAR_POSICAO  */
-    VAR = 264,                     /* VAR  */
-    PRINTLN = 265,                 /* PRINTLN  */
-    DB_EQUAL = 266,                /* DB_EQUAL  */
-    OR = 267,                      /* OR  */
-    IF = 268,                      /* IF  */
-    ELSE = 269,                    /* ELSE  */
-    AND = 270,                     /* AND  */
-    LOOP = 271,                    /* LOOP  */
-    STRING_LITERAL = 272,          /* STRING_LITERAL  */
-    SEMICOLON = 273,               /* SEMICOLON  */
-    LESS_THAN = 274,               /* LESS_THAN  */
-    GREATER_THAN = 275,            /* GREATER_THAN  */
-    EQUAL = 276,                   /* EQUAL  */
-    PLUS = 277,                    /* PLUS  */
-    MINUS = 278,                   /* MINUS  */
-    MULTIPLY = 279,                /* MULTIPLY  */
-    DIVIDE = 280,                  /* DIVIDE  */
-    STRING = 281,                  /* STRING  */
-    LPAREN = 282,                  /* LPAREN  */
-    RPAREN = 283,                  /* RPAREN  */
-    COMMA = 284,                   /* COMMA  */
-    LBRACE = 285,                  /* LBRACE  */
-    RBRACE = 286,                  /* RBRACE  */
-    NEWLINE = 287,                 /* NEWLINE  */
-    INT = 288,                     /* INT  */
-    IDEN = 289,                    /* IDEN  */
-    TIME = 290                     /* TIME  */
+    SUBMERGIR = 258,
+    EMERGIR = 259,
+    AJUSTAR_INCLINACAO = 260,
+    ATIVAR_PROPULSOR = 261,
+    AJUSTAR_PROFUNDIDADE = 262,
+    AJUSTAR_POSICAO = 263,
+    VAR = 264,
+    PRINTLN = 265,
+    DB_EQUAL = 266,
+    OR = 267,
+    IF = 268,
+    ELSE = 269,
+    AND = 270,
+    LOOP = 271,
+    STRING_LITERAL = 272,
+    SEMICOLON = 273,
+    LESS_THAN = 274,
+    GREATER_THAN = 275,
+    EQUAL = 276,
+    PLUS = 277,
+    MINUS = 278,
+    MULTIPLY = 279,
+    DIVIDE = 280,
+    STRING = 281,
+    LPAREN = 282,
+    RPAREN = 283,
+    COMMA = 284,
+    LBRACE = 285,
+    RBRACE = 286,
+    NEWLINE = 287,
+    INT = 288,
+    IDEN = 289,
+    TIME = 290
   };
-  typedef enum yytokentype yytoken_kind_t;
 #endif
 
 /* Value type.  */
@@ -100,7 +94,7 @@ union YYSTYPE
         int intValue;
         char* strValue;
 
-#line 104 "parser.tab.h"
+#line 98 "parser.tab.h"
 
 };
 typedef union YYSTYPE YYSTYPE;
@@ -111,8 +105,6 @@ typedef union YYSTYPE YYSTYPE;
 
 extern YYSTYPE yylval;
 
-
 int yyparse (void);
-
 
 #endif /* !YY_YY_PARSER_TAB_H_INCLUDED  */
